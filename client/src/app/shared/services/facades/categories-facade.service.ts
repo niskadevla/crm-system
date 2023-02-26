de2b24d67359a205b1fc741e04c6731455749fcd
niskadevla <niskadevla@gmail.com>
2023-02-26 16:47:17 +0100

    feat: create categories page and loader

---
+++ client/src/app/shared/services/facades/categories-facade.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { CategoriesService } from '../api-services/categories.service';
+import { ICategory } from '../../models/entities.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesFacade {
+
+  public get getAllCategories(): Observable<ICategory[]> {
+    return this.categoriesApi.getAll();
+  }
+
+  constructor(private categoriesApi: CategoriesService) { }
+}
