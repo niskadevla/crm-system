29f8aff54c1956ea7b724361b63fe66997aecbf1
niskadevla <niskadevla@gmail.com>
2023-02-28 16:46:21 +0100

    feat: create global loader

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
 import { AuthFacadeService } from './shared/services/facades/auth-facade.service';
+import { LoaderService } from './shared/services/loader.service';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
+  public spinner$: Observable<number> = this.loadService.spinner$;
 
-  constructor(private authFacadeService: AuthFacadeService) {
+  constructor(
+      private readonly authFacadeService: AuthFacadeService,
+      private readonly loadService: LoaderService
+  ) {
   }
 
   public ngOnInit(): void {
--- client/src/app/shared/components/loader/loader.component.ts
+++ client/src/app/shared/components/loader/loader.component.ts
@@ -9,5 +9,4 @@ import { CommonModule } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoaderComponent {
-
 }
