339c4243f4d49d1cc9315a9ec3990813c27cf1a8
niskadevla <niskadevla@gmail.com>
2023-03-01 13:56:53 +0100

    feat: be - create overview

--- server/src/models/order/order.models.ts
+++ server/src/models/order/order.models.ts
@@ -1,9 +1,9 @@
 import { OrdersModel } from './order.mongo';
 import { IOrder } from '../../entities';
-import { ExcludedQuery, getPagination, IQueryPagination, OrderEnum } from '../../utils';
+import { ExcludedQuery, getPagination, IQueryPagination, SortOrderEnum } from '../../utils';
 
 export const getLastOrderByFilter = async (filter: any) => {
-  return OrdersModel.findOne(filter).sort({ date: OrderEnum.Desc });
+  return OrdersModel.findOne(filter).sort({ date: SortOrderEnum.Desc });
 };
 
 export const createOrder = async (order: IOrder) => {
@@ -13,5 +13,9 @@ export const createOrder = async (order: IOrder) => {
 export const getOrdersByFilterWithPagination = async (filter: any, queryPagination: Partial<IQueryPagination>) => {
   const { skip, limit } = getPagination(queryPagination);
 
-  return OrdersModel.find(filter, ExcludedQuery).sort({ date: OrderEnum.Desc }).skip(skip).limit(limit);
+  return OrdersModel.find(filter, ExcludedQuery).sort({ date: SortOrderEnum.Desc }).skip(skip).limit(limit);
 };
+
+export const getAllOrdersByFilter = async (filter: any, direction: SortOrderEnum = SortOrderEnum.Asc) => {
+  return OrdersModel.find(filter).sort(direction);
+}
