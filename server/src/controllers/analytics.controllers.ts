86ee80428aa7e9e11012073b2870deceaec3ce48
niskadevla <niskadevla@gmail.com>
2023-03-01 15:23:15 +0100

    fix: be - clear response for analytics

--- server/src/controllers/analytics.controllers.ts
+++ server/src/controllers/analytics.controllers.ts
@@ -2,12 +2,14 @@ import { errorHandler, IGetRequest } from '../utils';
 import { getAllOrdersByFilter } from '../models/order/order.models';
 import { RevenueAnalytics } from '../models/analytics/revenue-analytics.class';
 import { IOrder } from '../entities';
+import { IRevenueAnalytics } from '../entities/interfaces/analytics.interfaces';
 
 export const httpOverview = async ({user}: IGetRequest, res: any) => {
   try {
-    const allOrders = await getAllOrdersByFilter({user: user?.id})
+    const allOrders = await getAllOrdersByFilter({user: user?.id});
+    const {revenue, orders}: IRevenueAnalytics = new RevenueAnalytics(allOrders as IOrder[]);
 
-    res.status(200).json(new RevenueAnalytics(allOrders as IOrder[]));
+    return res.status(200).json({revenue, orders});
   } catch (err: unknown) {
     return errorHandler(res, err as Error);
   }
