d47069bcf11c5283485c011e7bd6e91126bf6d78
niskadevla <niskadevla@gmail.com>
2023-03-01 15:24:51 +0100

    feat: fe - create functionality for overview page

--- client/src/app/core/overview-page/overview-page.component.ts
+++ client/src/app/core/overview-page/overview-page.component.ts
@@ -1,5 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { Observable } from 'rxjs';
+
+import { IRevenueAnalytics } from '../../shared/models/entities.models';
+import { AnalyticsFacade } from '../../shared/services/facades/analytics-facade.service';
+import { MaterialInstance, MaterialService } from '../../shared/services/material.service';
+import { DATE_FORMAT } from '../../shared/constants/date.constants';
+import { CURRENCY } from '../../shared/constants/common.constants';
 
 @Component({
   selector: 'app-overview-page',
@@ -9,6 +25,45 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./overview-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OverviewPageComponent {
+export class OverviewPageComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('tapTarget') private readonly tapTargetRef!: ElementRef;
+
+  public readonly dateFormat = DATE_FORMAT;
+  public readonly currency = CURRENCY;
+
+  public tapTarget!: MaterialInstance;
+  public overviewData$!: Observable<IRevenueAnalytics>;
+  public yesterday: Date = new Date();
+
+  constructor(
+      private analyticsFacade: AnalyticsFacade,
+      private materialService: MaterialService
+  ) {
+  }
+
+  public ngOnInit(): void {
+    this.getOverviewData();
+    this.setYesterday();
+  }
+
+  public ngOnDestroy(): void {
+    this.tapTarget.destroy();
+  }
+
+  public ngAfterViewInit(): void {
+    this.tapTarget = this.materialService.initTapTarget(this.tapTargetRef);
+  }
+
+  public openInfo(): void {
+    this.tapTarget.open();
+  }
+
+  private getOverviewData(): void {
+    this.overviewData$ = this.analyticsFacade.getOverview();
+  }
+
+  private setYesterday(): void {
+    this.yesterday.setDate(new Date().getDate() - 1);
+  }
 
 }
--- client/src/app/shared/models/entities.models.ts
+++ client/src/app/shared/models/entities.models.ts
@@ -35,3 +35,15 @@ export interface IFilter {
   end?: Date;
   order?: number;
 }
+
+export interface IRevenueAnalytics {
+  revenue: IRevenueStatistic;
+  orders: IRevenueStatistic;
+}
+
+export interface IRevenueStatistic {
+  percent: number;
+  compare: number;
+  yesterday: number;
+  isHigher: boolean;
+}
