7b453a84b4bb15de3d17fe53d5ea00625909c175
niskadevla <niskadevla@gmail.com>
2023-03-08 14:10:13 +0100

    refact: move middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,23 +1,19 @@
 import express from 'express';
-import morgan from 'morgan';
 import path from 'path';
 
 import { useAuthentication } from './middleware/authentication/passport/passport';
 import { useSecure } from './middleware/secure/use-secure';
-import { ENV_CONFIG } from './env-config';
-import { authRoutes, analyticsRoutes, categoryRoutes, orderRoutes, positionRoutes } from './routes';
+import { useLogger } from './middleware/logger/use-logger';
+import { useApiRouter } from './middleware/api-router/use-api-router';
 
 export const app = express();
 
+useLogger(app);
 useSecure(app);
 useAuthentication(app);
 
 app.use('/uploads', express.static(path.join(__dirname, '../', 'uploads')));
-app.use(morgan('combined'));
+
 app.use(express.json());
 
-app.use(`${ENV_CONFIG.API_URL}/auth`, authRoutes);
-app.use(`${ENV_CONFIG.API_URL}/analytics`, analyticsRoutes);
-app.use(`${ENV_CONFIG.API_URL}/category`, categoryRoutes);
-app.use(`${ENV_CONFIG.API_URL}/order`, orderRoutes);
-app.use(`${ENV_CONFIG.API_URL}/position`, positionRoutes);
+useApiRouter(app);
--- server/src/utils/interfaces/env-config.interfaces.ts
+++ server/src/utils/interfaces/env-config.interfaces.ts
@@ -1,4 +1,7 @@
+import { NodeEnvEnums } from '../enums';
+
 export interface EnvironmentConfig {
+  env: NodeEnvEnums,
   mongoURI: string;
   JWT: string;
   API_URL: string;
